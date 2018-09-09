85be75fc55fc6a2b62935561d2aa745ff5dbbea9
NPVU <npvucusc@gmail.com>
2018-09-09 22:40:33 +0700

    Chỉnh sửa các trang error 401 404

--- resources/views/errors/401.blade.php
+++ resources/views/errors/401.blade.php
@@ -1,11 +1,10 @@
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <link href="{{ asset('public/template/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('public/template/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
-    </head>
-    <body class="skin-blue sidebar-mini" style="height: auto; min-height: 100%;">
-        <section class="content">
-            <div class="error-page">
+@extends('layouts.app') 
+@section('title')
+    401
+@endsection 
+@section('contentLeft')
+<div class="content-left-section" >
+<div class="error-page">
                 <h2 class="headline text-red"> 401</h2>
 
                 <div class="error-content">
@@ -18,7 +17,5 @@
                 </div>
                 <!-- /.error-content -->
             </div>
-            <!-- /.error-page -->
-        </section>
-    </body>
-</html>
+</div>
+@endsection
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,11 +1,10 @@
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <link href="{{ asset('public/template/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
-        <link href="{{ asset('public/template/AdminLTE.min.css') }}" rel="stylesheet" type="text/css" />
-    </head>
-    <body>
-        <section class="content">
-            <div class="error-page">
+@extends('layouts.app') 
+@section('title')
+    404
+@endsection 
+@section('contentLeft')
+<div class="content-left-section" >
+<div class="error-page">
                 <h2 class="headline text-yellow">404</h2>
 
                 <div class="error-content">
@@ -18,7 +17,5 @@
                 </div>
                 <!-- /.error-content -->
             </div>
-            <!-- /.error-page -->
-        </section>
-    </body>
-</html>
+</div>
+@endsection
