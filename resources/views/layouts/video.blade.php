f11dab51c37974253ae04d7e525143e791c512ec
NPVU <npvucusc@gmail.com>
2018-09-25 13:08:10 +0700

    Sửa lỗi khi xem server youtube

--- resources/views/layouts/video.blade.php
+++ resources/views/layouts/video.blade.php
@@ -21,14 +21,14 @@
             </a>
         </p>
 </video>
-@elseif(strcmp($_GET['s'], md5('youtube'))==0)
+@elseif(strcmp($_GET['s'], md5('youtube'))==0) 
     @if(!empty($tap[0]->tap_youtubelink))
     <iframe class="npv-youtube" src="{{$tap[0]->tap_youtubelink}}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="border: 1px solid white" width="100%" height="380"></iframe>
     @else    
     <script>
         window.location.href = $('meta[name="url"]').attr('content')+'/xem-phim/'+"{{strtolower(str_replace('/','-',str_replace(' ', '-',ClassCommon::removeVietnamese($phim[0]->phim_ten))))}}/?pid="+getParameterByName('pid','')+"&t="+getParameterByName('t','')+"&s={{md5('google')}}";
     </script>
-    @endif
+    @endif 
 @else
 <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center" style="margin-top: 20px;">
     <h4 style="color:red">Vui lòng không tự ý sửa đường dẫn trên trình duyệt</h4>
