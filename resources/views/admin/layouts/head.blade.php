ef804e1aff1cc1336fbba3e53e4bce37d8cfb487
NPVU <npvucusc@gmail.com>
2018-09-25 22:31:15 +0700

    sửa lỗi mất js

--- resources/views/admin/layouts/head.blade.php
+++ resources/views/admin/layouts/head.blade.php
@@ -12,6 +12,7 @@
     <script type="text/javascript" src="{{ asset('template/plugins/iCheck/icheck.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('template/adminlte.min.js') }}"></script>
     <script type="text/javascript" src="{{ asset('js/dropzone.js') }}"></script>     
+    <script type="text/javascript" src="{{ asset('js/main.min.js') }}"></script>     
     <link href="{{ asset('template/bower_components/bootstrap/dist/css/bootstrap.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('template/bower_components/font-awesome/css/font-awesome.min.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('template/bower_components/Ionicons/css/ionicons.min.css') }}" rel="stylesheet" type="text/css" />
