ce3efc599e4110b6221dd272748855e7d4c529df
NPVU <npvucusc@gmail.com>
2018-10-05 23:02:18 +0700

    Sửa lỗi css hiển thị đè thông tin phim

--- resources/views/xemphim.blade.php
+++ resources/views/xemphim.blade.php
@@ -378,7 +378,7 @@
                                     </ul>
                                 </div>
                             </div>
-                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-top:-50px;">
+                            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12" style="margin-top:-30px;">
                                 <ul style="list-style: none;padding-left: 0px;">                            
                                     <li>
                                         <label>Nguồn video:</label>
