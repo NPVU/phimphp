3f6e3d522ee3ff8ee347c40c0ef23ad680822940
NPVU <npvucusc@gmail.com>
2018-11-10 13:34:01 +0700

    Cập nhật ngày 10/11/2018 13:31
    - Sửa footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -11,17 +11,9 @@
 <div class="footer">
     <div class="container" style="padding: 15px;">        
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-            <div class="" style="float:left;">
+            <div class="text-center">
                 <span>© 2018 &nbsp;{{session('website_name')}}. All rights reserved | &nbsp;{{session('website_email')}}</span>
-            </div>
-            <div class="" style="float:right;cursor:pointer">
-                <span class="fas fa-laptop" title="Hỗ trợ tốt hơn trên máy tính" style="font-size: 1.3em"></span>
-                <span title="Xem tốt trên trình duyệt Google Chrome, Cốc Cốc, Firefox" >
-                    <span class="fab fa-chrome" style="font-size: 1.3em"></span>
-                    <span class="fab fa-firefox" style="font-size: 1.3em"></span>
-                </span>                
-                <span>Version &nbsp;{{session('website_version')}} </span>
-            </div>
+            </div>            
             <div class="clearfix"></div>
         </div>
     </div>
